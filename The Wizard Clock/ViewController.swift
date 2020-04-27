b1378e8f73dd29b1392ad030e9ac454d3d53ccf8
dinerd <dinerd@oregonstate.edu>
2020-04-26 21:00:23 -0400

    Added more images

--- The Wizard Clock/ViewController.swift
+++ The Wizard Clock/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let mystring = "wassup?"
+        let mystring = "dude?"
         
         print(mystring)
     }
